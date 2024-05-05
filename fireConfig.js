754ac0cb66f8563cd3195695220ecb009608dd2a
BodaSrikanth <boyasrikanth10@gmail.com>
2024-05-05 18:11:59 +0530

    first commit

---
+++ fireConfig.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDny0LPH6XNP2SqqZsHQX5YwnwO4hmZgJI",
+  authDomain: "stundentappl.firebaseapp.com",
+  databaseURL: "https://stundentappl-default-rtdb.firebaseio.com",
+  projectId: "stundentappl",
+  storageBucket: "stundentappl.appspot.com",
+  messagingSenderId: "52191755178",
+  appId: "1:52191755178:web:92c6f020b148f520ebc9d3",
+  measurementId: "G-89NLYEKW81"
+};
+
+
+firebase.initializeApp(firebaseConfig);
---
+++ myscript.js
@@ -0,0 +1,70 @@
+var rollV, nameV, genderV, addressV;
+
+function readFom() {
+  rollV = document.getElementById("roll").value;
+  nameV = document.getElementById("name").value;
+  genderV = document.getElementById("gender").value;
+  addressV = document.getElementById("address").value;
+  console.log(rollV, nameV, addressV, genderV);
+}
+
+
+
+document.getElementById("insert").onclick = function () {
+  readFom();
+
+    firebase.database().ref("student/" + rollV).set({
+        rollNo: rollV,
+        name: nameV,
+        gender: genderV,
+        address: addressV,
+    });
+  alert("Data Inserted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+};
+
+document.getElementById("read").onclick = function () {
+  readFom();
+
+  firebase.database().ref("student/" + rollV).on("value", function (snapshot) {
+      document.getElementById("roll").value = snapshot.val().rollNo;
+      document.getElementById("name").value = snapshot.val().name;
+      document.getElementById("gender").value = snapshot.val().gender;
+      document.getElementById("address").value = snapshot.val().address;
+    });
+};
+
+document.getElementById("update").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .update({
+      //   rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
+    });
+  alert("Data Update");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+};
+document.getElementById("delete").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .remove();
+  alert("Data Deleted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+};
